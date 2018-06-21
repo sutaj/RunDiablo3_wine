2af82e97650e62d0418f41a243b8bcd88ee1bad1
Akil <krs.akil@gmail.com>
2018-06-21 20:34:04 +0200

    Add files via upload

---
+++ run_diablo.sh
@@ -0,0 +1,5 @@
+xrandr --output DVI-D-0 --off
+cd '~/.wine/drive_c/Program Files (x86)/Diablo III/'
+/usr/bin/setarch i386 -3 -L -B -R wine '~/.wine/drive_c/Program Files (x86)/Diablo III/Diablo III.exe' -launch -opengl
+xrandr --output DVI-D-0 --auto --right-of DVI-I-1
+
