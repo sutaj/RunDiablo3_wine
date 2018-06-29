63ed490c88e1d89d74c8243f7c1f169c1665b7d5
Akil <krs.akil@gmail.com>
2018-06-29 21:59:16 +0200

    Update run_diablo.sh

--- run_diablo.sh
+++ run_diablo.sh
@@ -1,4 +1,4 @@
 xrandr --output DVI-D-0 --off
-cd 'home/$USER/.wine/drive_c/Program Files (x86)/Diablo III/'
-/usr/bin/setarch i386 -3 -L -B -R wine 'home/$USER/.wine/drive_c/Program Files (x86)/Diablo III/Diablo III.exe' -launch -opengl
+cd '$HOME/.wine/drive_c/Program Files (x86)/Diablo III/'
+/usr/bin/setarch i386 -3 -L -B -R wine '$HOME/.wine/drive_c/Program Files (x86)/Diablo III/Diablo III.exe' -launch -opengl
 xrandr --output DVI-D-0 --auto --right-of DVI-I-1
